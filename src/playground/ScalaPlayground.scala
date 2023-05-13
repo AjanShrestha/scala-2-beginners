bc76008172a37f15828635cf083aca9354266d1b
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-05-14 08:19:46 +1000

    setup: Setup the project and test jdk and scala setup

---
+++ src/playground/ScalaPlayground.scala
@@ -0,0 +1,17 @@
+package playground
+
+// There are 2 ways to create Scala App
+// 1. by extending App
+// provides the main method
+// and makes the body executable
+object ScalaPlayground extends App {
+  println("Hello, Scala!")
+}
+
+// 2. Write the main method
+// Creates a standalone app
+//object ScalaPlayground {
+//  def main(args: Array[String]): Unit = {
+//    println("Hello, Scala!")
+//  }
+//}
