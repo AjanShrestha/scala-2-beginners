9327d7e2b01bd24ba2e9b7205ebeb30e1a2b89f8
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-05-16 20:37:25 +1000

    feat: Anonymous Functions
    Signed-off-by: Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>

---
+++ src/lectures/part3fp/AnonymousFunctions.scala
@@ -0,0 +1,35 @@
+package lectures.part3fp
+
+object AnonymousFunctions extends App {
+
+  // anonymous function (LAMBDA)
+  val doubler: Int => Int = x => x * 2
+
+  // multiple params in a lambda
+  val adder: (Int, Int) => Int = (a: Int, b: Int) => a + b
+
+  // no params
+  private val justDoSomething: () => Int = () => 3
+
+  // careful
+  println(justDoSomething) // function itself
+  println(justDoSomething()) // call
+
+  // curly braces with lambdas
+  val stringToInt = { (str: String) =>
+    str.toInt
+  }
+
+  // MORE syntactic sugar
+  val niceIncrementer: Int => Int = _ + 1 // equivalent to x => x + 1
+  val niceAdder: (Int, Int) => Int = _ + _ // equivalent to (a,b) => a + b
+
+  /*
+    1.  MyList: replace all FunctionX calls with lambdas
+    2.  Rewrite the "special" adder as an anonymous function
+   */
+
+  private def superAdd: Int => Int => Int = x => y => x + y
+
+  println(superAdd(3)(4))
+}
